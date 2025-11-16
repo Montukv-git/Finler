98f750451fc910dd7373b464580dc4bc9a4f77b3
Montukeshwar Vaishnaw <montukeshwar20@gmail.com>
2025-11-16 20:58:53 +0530

    Clean up settings.py by removing comments and whitespace
    Removed commented-out sections and unnecessary whitespace from settings.py.

--- Finler/settings.py
+++ Finler/settings.py
@@ -1,24 +1,11 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'li$41glc5w&uwct%7*n-w46$kct@9^#8m1e+=kxdkwbnf_r5&('
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
-
 INSTALLED_APPS = [
     
     'django.contrib.admin',
@@ -66,20 +53,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Finler.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -95,10 +74,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -121,7 +96,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-#for user registratoin form
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
@@ -133,10 +107,8 @@ EMAIL_HOST_USER = 'finler.official@gmail.com'
 EMAIL_HOST_PASSWORD = 'Finler998MAH$'
 EMAIL_PORT = 587
 
-#for restriction user to access page if not logged in
 LOGIN_URL = "/login"
 
-#for unique fields in database
 AUTHENTICATION_BACKENDS = [
      'django.contrib.auth.backends.ModelBackend'
 ]
